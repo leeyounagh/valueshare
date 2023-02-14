0444b8357cb77bfef04092765f39fe67b13257f3
[IoT1]양효빈01~ <yangnorong@gmail.com>
2023-02-14 23:16:05 +0900

    color 스타일가이드 추가

--- src/styles/color.js
+++ src/styles/color.js
@@ -1,6 +1,15 @@
 const color = {
   black: "#000000",
   white: "#ffffff",
+  main: "#ff985f",
+  green:"#27ae60",
+  red:"#eb5757",
+  gray1:"333333",
+  gray2:"4f4f4f",
+  gray3:"828282",
+  gray4:"bdbdbd",
+  gary5:"e0e0e0",
+  gray6:"#f2f2f2"
 };
 
 export default color;
