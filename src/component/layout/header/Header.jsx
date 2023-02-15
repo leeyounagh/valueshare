cbde3e32df85a07395d23344cb1964fb13dc6bd9
[IoT1]양효빈01~ <yangnorong@gmail.com>
2023-02-15 14:55:02 +0900

    header,eslint,footer커밋 에러 수정, asset 추가

--- src/component/layout/header/Header.jsx
+++ src/component/layout/header/Header.jsx
@@ -1,19 +1,207 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import color from "../../../styles/color";
 
-const { white } = color;
+const { white, gray4 } = color;
 
 const SlayOut = styled.header`
-  width: 1920px;
+  width: 100%;
   height: 140px;
   margin: 0 0 16px;
   padding: 35px 0 0;
   background-color: ${white};
+  border-bottom: 1px solid ${gray4};
+  display: flex;
+  justify-content: center;
+  a {
+    text-decoration: none;
+    border: black;
+    font-family: "Montserrat", "Noto Sans KR", "Rufina", "sans-serif";
+  }
+`;
+const SLogoDiv = styled.div`
+  width: 20%;
+  height: 100%;
+  font-size: 45px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: black;
+  white-space: nowrap;
+  a {
+    color: black;
+  }
+`;
+const SBestSellerDiv = styled.div`
+  height: 100%;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
 `;
 
+const SProductDiv = styled.div`
+  height: 100%;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+`;
+
+const SLookBookDiv = styled.div`
+  height: 100%;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+`;
+
+const SMembershipDiv = styled.div`
+  height: 100%;
+  width: 130px;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+`;
+const SLineDiv = styled.div`
+  width: 1px;
+  height: 60px;
+  transform: rotate(-180deg);
+  background-color: #ccd1d8;
+`;
+const SLayoutInnerDiv = styled.div`
+  display: flex;
+  height: 67px;
+  width: 100%;
+  padding-left: 40px;
+`;
+const SLayoutMenuDiv = styled.div`
+  width: 60%;
+  height: 67px;
+  display: flex;
+  justify-content: center;
+  padding-top: 25px;
+`;
+const SLayoutIconDiv = styled.div`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  width: 25%;
+  padding-top: 10px;
+  margin-left: 60px;
+`;
+const SAlarmImg = styled.img`
+  width: 46px;
+  height: 40px;
+  object-fit: contain;
+`;
+const SBasketImg = styled.img`
+  width: 46px;
+  height: 40px;
+  object-fit: contain;
+`;
+const SProfileImg = styled.img`
+  width: 70px;
+  height: 60px;
+`;
+const SProfileMenuImg = styled.img`
+  width: 26px;
+  height: 30px;
+  margin-left: 10px;
+  margin-top: 15px;
+  object-fit: contain;
+`;
+const SLayoutIconItem = styled.div`
+  margin: 5px;
+
+  height: 100%;
+  width: 20%;
+`;
+const SInnerItemDiv = styled.div`
+  justify-content: space-between;
+  width: 60%;
+  height: 100%;
+  display: flex;
+`;
+const SLayoutProfileDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50%;
+  height: 100%;
+  margin-bottom: 20px;
+`;
+const SLayoutProfileInnerDiv = styled.div`
+  width: 25%;
+`;
 function Header() {
-  return <SlayOut>Header</SlayOut>;
+  return (
+    <SlayOut>
+      <SLayoutInnerDiv>
+        <SLogoDiv>
+          <Link to="/">Value shop</Link>
+        </SLogoDiv>
+
+        <SLayoutMenuDiv>
+          <SInnerItemDiv>
+            <Link to="/bestseller">
+              <SBestSellerDiv>Best seller</SBestSellerDiv>
+            </Link>
+            <Link to="/product">
+              <SProductDiv>Products</SProductDiv>
+            </Link>
+            <Link to="/lookbook">
+              <SLookBookDiv>lookbook</SLookBookDiv>
+            </Link>
+            <Link to="/Membership">
+              <SMembershipDiv>Membership</SMembershipDiv>
+            </Link>
+          </SInnerItemDiv>
+        </SLayoutMenuDiv>
+        <SLayoutIconDiv>
+          <SLayoutIconItem>
+            <SAlarmImg src="asset/headerAlarm.svg" />
+          </SLayoutIconItem>
+          <SLayoutIconItem>
+            <SBasketImg src="asset/icn-basket.svg" />
+          </SLayoutIconItem>
+          <SLineDiv />
+          <SLayoutProfileDiv>
+            <SLayoutProfileInnerDiv>
+              <SProfileImg src="asset/icn-profile.svg" />
+            </SLayoutProfileInnerDiv>
+            <SLayoutProfileInnerDiv>
+              <SProfileMenuImg src="asset/Chevrons_chevron-right.svg" />
+            </SLayoutProfileInnerDiv>
+          </SLayoutProfileDiv>
+        </SLayoutIconDiv>
+      </SLayoutInnerDiv>
+    </SlayOut>
+  );
 }
 
 export default Header;
--- src/pages/admin/management/category/uploadimage/UploadImage.jsx
+++ src/pages/admin/management/category/uploadimage/UploadImage.jsx
@@ -6,53 +6,53 @@ import { useDispatch } from "react-redux";
 import { productImage } from "../../../../../slice/ImageSlice";
 
 function UploadImage() {
-  const [image, setImage] = useState([]);
-  const dispatch = useDispatch();
+	const [image, setImage] = useState([]);
+	const dispatch = useDispatch();
 
-  const handleDrop = async (files) => {
+	const handleDrop = async (files) => {
     const formData = new FormData();
-    formData.append("file", files[0]);
-    console.log(files[0]);
+		formData.append("file", files[0]);
+		console.log(files[0]);
 
-    try {
-      const response = await axios.post("요청주소", formData);
-      const data = await response.data;
-      setImage((prev) => [...prev, ...data]);
-      dispatch(productImage(image));
-    } catch (err) {
-      alert("이미지 업로드에 실패했습니다.");
-      console.log(err);
-    }
-  };
+		try {
+			const response = await axios.post("요청주소", formData);
+			const data = await response.data;
+			setImage((prev) => [...prev, ...data]);
+			dispatch(productImage(image));
+		} catch (err) {
+			alert("이미지 업로드에 실패했습니다.");
+			console.log(err);
+		}
+	};
 
-  return (
-    <div>
-      <Dropzone onDrop={handleDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div
-              style={{
-                width: 250,
-                height: 240,
-                border: "1px solid black",
-                borderRadius: "10px",
-              }}
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              {...getRootProps()}
-            >
-              <input {...getInputProps()} />
-              이미지 업로드
-            </div>
-          </section>
-        )}
-      </Dropzone>
-      <div>
-        {image.map((item) => {
-          return <img src={item.name} alt={item.name} />;
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Dropzone onDrop={handleDrop}>
+				{({ getRootProps, getInputProps }) => (
+					<section>
+						<div
+							style={{
+								width: 250,
+								height: 240,
+								border: "1px solid black",
+								borderRadius: "10px",
+							}}
+							// eslint-disable-next-line react/jsx-props-no-spreading
+							{...getRootProps()}
+						>
+							<input {...getInputProps()} />
+							이미지 업로드
+						</div>
+					</section>
+				)}
+			</Dropzone>
+			<div>
+				{image.map((item) => {
+					return <img key={item.id} src={item.name} alt={item.name} />;
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default UploadImage;
