a2a9aff99801fc40cfd05a72bd35ba6cb680f751
[IoT1]이수연 <dudgk0216@gmail.com>
2023-02-16 17:20:04 +0900

    feat 이미지 업로드 미리보기 구현

--- src/component/uploadimage/UploadImage.jsx
+++ src/component/uploadimage/UploadImage.jsx
@@ -1,47 +1,35 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from "react";
-import Dropzone from "react-dropzone";
-import { useDispatch } from "react-redux";
-import { productImage } from "slice/ImageSlice";
+import React, { useRef, useState } from "react";
+// import Dropzone from "react-dropzone";
+// import axios from "axios";
+// import { useDispatch } from "react-redux";
+// import { productImage } from "slice/ImageSlice";
 
 function UploadImage() {
-  // const [image, setImage] = useState([]);
-  const dispatch = useDispatch();
+  const [imgFile, setImgFile] = useState("");
+  const imgRef = useRef();
 
-  const handleDrop = async (files) => {
-    console.log(files[0]);
-
-    // const formData = new FormData();
-    // formData.append("file", files[0]);
-    // console.log(files[0]);
-    dispatch(productImage(files[0]));
+  const saveImgFile = () => {
+    const file = imgRef.current.files[0];
+    console.log(imgRef.current.files[0]);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImgFile(reader.result);
+    };
   };
 
   return (
     <div>
-      <Dropzone onDrop={handleDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div
-              style={{
-                width: 250,
-                height: 240,
-                border: "1px solid black",
-                borderRadius: "10px",
-              }}
-              {...getRootProps()}
-            >
-              <input {...getInputProps()} />
-              이미지 업로드
-            </div>
-          </section>
-        )}
-      </Dropzone>
-
       <div>
-        {/* {image.map((item) => {
-          return <img key={item.id} src={item.name} alt={item.name} />;
-        })} */}
+        <img src={imgFile} alt="상품이미지" />
+        <input
+          type="file"
+          accept="image/*"
+          id="profileImg"
+          onChange={saveImgFile}
+          ref={imgRef}
+        />
       </div>
     </div>
   );
