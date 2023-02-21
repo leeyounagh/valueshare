a6a63b7599248b15aaa3cd1ab781b6526c25ca91
[IoT1]양효빈01~ <yangnorong@gmail.com>
2023-02-21 18:51:15 +0900

    style: nav bar 수정

--- src/component/Navbar2.jsx
+++ src/component/Navbar2.jsx
@@ -5,27 +5,25 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import color from "../styles/color";
 
-const { gray4 } = color;
-
 const SLayout = styled.nav`
   width: 100%;
   height: 100px;
   padding: 33px 0 0;
   background-color: var(--white);
-  border-bottom: 1px solid ${gray4};
+  border-bottom: 1px solid ${color.gray4};
 `;
 
 const SRootHomeDiv = styled.div`
-  display: flex;
   align-items: center;
   width: 50px;
   height: 100%;
+  margin: 0 10px;
 `;
 const SRootTitleDiv = styled.div`
-  display: flex;
   justify-content: center;
   align-items: center;
   width: 60px;
+  margin: 0 10px;
 `;
 const SHomeImg = styled.img`
   width: 28px;
@@ -46,7 +44,7 @@ const SRootDiv = styled.div`
   line-height: normal;
   letter-spacing: normal;
   text-align: left;
-  color: #000;
+  margin: 0 10px;
 `;
 const SRootIconDiv = styled.div`
   height: 100%;
@@ -62,13 +60,11 @@ const SRootIcontImg = styled.img`
 const SCategoryDiv = styled.div``;
 const SCategoryTitle = styled.div`
   height: 100%;
-  display: flex;
   align-items: center;
 `;
 const SPageTitleDiv = styled.div`
   width: 100%;
-  height: 70%;
-  margin-left: 54px;
+  height: 100%;
   font-size: 24px;
   font-weight: bold;
   font-stretch: normal;
@@ -76,7 +72,6 @@ const SPageTitleDiv = styled.div`
   line-height: normal;
   letter-spacing: normal;
   text-align: left;
-  color: #000;
 `;
 function Navbar2() {
   const text = useSelector((state) => {
@@ -101,21 +96,24 @@ function Navbar2() {
     <SLayout>
       <SRootDiv>
         <SRootHomeDiv>
-          <SHomeImg src="../../public/asset/icon-home.svg" />
+          <SHomeImg src="../asset/icon-home.svg" />
         </SRootHomeDiv>
         <SRootTitleDiv>
           <SRootTitle>Home</SRootTitle>
         </SRootTitleDiv>
         <SRootIconDiv>
-          <SRootIcontImg src="asset/Chevrons_chevron-right.svg" />
+          <SRootIcontImg src="../asset/Chevrons_chevron-right.svg" />
         </SRootIconDiv>
         <SCategoryDiv>
           <SCategoryTitle>{text.rootTitle}</SCategoryTitle>
         </SCategoryDiv>
         <SRootIconDiv>
-          <SRootIcontImg src="asset/Chevrons_chevron-right.svg" />
+          <SRootIcontImg src="../asset/Chevrons_chevron-right.svg" />
         </SRootIconDiv>
-        <SPageTitleDiv>{text.currentTitle}</SPageTitleDiv>
+        <SCategoryTitle>{text.currentTitle}</SCategoryTitle>
+        <SRootIconDiv>
+          <SRootIcontImg src="../asset/Chevrons_chevron-right.svg" />
+        </SRootIconDiv>
         <SPageTitleDiv>{productInformation?.result.productTitle}</SPageTitleDiv>
       </SRootDiv>
     </SLayout>
