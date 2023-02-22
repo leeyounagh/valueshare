388ddce956490e858d60c1322150a8817e018bdf
hyojeong <777hanhyo@gmail.com>
2023-02-22 13:46:43 +0900

    feat: 회원가입, 로그인

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import Footer from "component/layout/footer/Footer";
-import UserInfo from "pages/userinfo/UserInfo";
-import OrderedList from "pages/orderedlist/OrderedList";
-import Header from "component/layout/header/Header";
-import History from "pages/history/History";
-import Login from "pages/login/Login";
-import Main from "pages/main/Main";
-import Mypage from "pages/mypage/MyPage";
-import Register from "pages/register/Register";
-import BestSeller from "pages/bestseller/BestSeller";
-import Cart from "pages/cart/Cart";
-import Detail from "pages/productdeail/Detail";
-import Admin from "pages/admin/Admin";
-import Resign from "pages/resign/Resign";
-import DeliveryList from "pages/deliverylist/DeliveryList";
-import CanceledList from "pages/canceledlist/CanceledList";
-import UploadProduct from "pages/uploadproduct/UploadProduct";
-import DeleteProduct from "pages/deleteproduct/DeleteProduct";
-import EditProduct from "pages/editproduct/EditProduct";
-import LookBook from "pages/lookbook/LookBook";
-import Membership from "pages/membership/Membership";
-import Product from "pages/products/Product";
-import NoneMember from "pages/nonmember/NoneMember";
-import { setPageInfo } from "slice/PageSlice";
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Footer from 'component/layout/footer/Footer';
+import UserInfo from 'pages/userinfo/UserInfo';
+import OrderedList from 'pages/orderedlist/OrderedList';
+import Header from 'component/layout/header/Header';
+import History from 'pages/history/History';
+import Login from 'pages/login/Login';
+import Main from 'pages/main/Main';
+import Mypage from 'pages/mypage/MyPage';
+import Register from 'pages/register/Register';
+import BestSeller from 'pages/bestseller/BestSeller';
+import Cart from 'pages/cart/Cart';
+import Detail from 'pages/productdeail/Detail';
+import Admin from 'pages/admin/Admin';
+import Resign from 'pages/resign/Resign';
+import DeliveryList from 'pages/deliverylist/DeliveryList';
+import CanceledList from 'pages/canceledlist/CanceledList';
+import UploadProduct from 'pages/uploadproduct/UploadProduct';
+import DeleteProduct from 'pages/deleteproduct/DeleteProduct';
+import EditProduct from 'pages/editproduct/EditProduct';
+import LookBook from 'pages/lookbook/LookBook';
+import Membership from 'pages/membership/Membership';
+import Product from 'pages/products/Product';
+import NoneMember from 'pages/nonmember/NoneMember';
+import { setPageInfo } from 'slice/PageSlice';
 
 function App() {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
-  if (pathname === "/product") {
-    dispatch(setPageInfo({ rootTitle: "Category", currentTitle: "Products" }));
+  if (pathname === '/product') {
+    dispatch(setPageInfo({ rootTitle: 'Category', currentTitle: 'Products' }));
   }
-  if (pathname === "/cart") {
+  if (pathname === '/cart') {
     dispatch(
-      setPageInfo({ rootTitle: "Shopping Bag", currentTitle: "Shopping Bag" })
+      setPageInfo({ rootTitle: 'Shopping Bag', currentTitle: 'Shopping Bag' })
     );
   }
-  if (pathname === "/membership") {
+  if (pathname === '/membership') {
     dispatch(
-      setPageInfo({ rootTitle: "Membership", currentTitle: "Membership" })
+      setPageInfo({ rootTitle: 'Membership', currentTitle: 'Membership' })
     );
   }
-  if (pathname === "/lookbook") {
-    dispatch(setPageInfo({ rootTitle: "LookBook", currentTitle: "LookBook" }));
+  if (pathname === '/lookbook') {
+    dispatch(setPageInfo({ rootTitle: 'LookBook', currentTitle: 'LookBook' }));
   }
-  if (pathname === "/nonemember") {
-    dispatch(setPageInfo({ rootTitle: "Order", currentTitle: "Order" }));
+  if (pathname === '/nonemember') {
+    dispatch(setPageInfo({ rootTitle: 'Order', currentTitle: 'Order' }));
   }
 
   return (
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
 const SLayout = styled.div`
   border: 1px solid black;
@@ -11,20 +12,131 @@ const SLayout = styled.div`
 `;
 
 function Register() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [phone, setPhone] = useState('');
+
+  console.log(email, name, password, confirmPassword);
+
+  const onEmailHandler = (e) => {
+    const email = e.target.value;
+    const isEmailValid = email.includes('@');
+
+    if (!isEmailValid) {
+      return errorMessage;
+    }
+    setEmail(email);
+  };
+  const onNameHandler = (e) => {
+    setName(e.target.value);
+  };
+  const onPasswordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+  const onConfirmPasswordHandler = (e) => {
+    if (password !== confirmPassword) setConfirmPassword(e.target.value);
+  };
+  const onPhoneHandler = (e) => {
+    const phoneNumber = e.target.value.replace(/[^0-9]/g, '').split('');
+
+    const arr = [
+      [0, phoneNumber > 9 ? 3 : 2],
+      [0, phoneNumber > 10 ? 4 : 3],
+      [0, 4],
+    ];
+
+    e.target.value = arr
+      .map(function (v) {
+        return phoneNumber.splice(v[0], v[1]).join('');
+      })
+      .filter(function (t) {
+        return t;
+      })
+      .join('-');
+
+    setPhone(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      return alert('비밀번호를 확인해주세요');
+    }
+
+    const body = {
+      email,
+      name,
+      password,
+      phone,
+    };
+
+    axios
+      .post('', body)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setError('에러');
+      });
+  };
+
   return (
     <SLayout>
-      <form>
-        <div style={{ marginBottom: "50px" }}>
-          <input placeholder="이메일을 입력해주세요" />
+      <h1>회원가입</h1>
+      <form onSubmit={onSubmitHandler}>
+        <div style={{ marginBottom: '50px' }}>
+          <label>이메일</label>
+          <input
+            name="email"
+            value={email}
+            onChange={onEmailHandler}
+            placeholder="이메일을 입력해주세요"
+          />
+          {}
         </div>
-        <div style={{ marginBottom: "50px" }}>
-          <input placeholder="비밀번호를 입력해주세요" />
+        <div style={{ marginBottom: '50px' }}>
+          <label>이름</label>
+          <input
+            name="name"
+            value={name}
+            onChange={onNameHandler}
+            placeholder="이름을 입력해주세요"
+          />
         </div>
-        <div style={{ marginBottom: "50px" }}>
-          <input placeholder="비밀번호 확인" />
+        <div style={{ marginBottom: '50px' }}>
+          <label>비밀번호</label>
+          <input
+            name="password"
+            value={password}
+            onChange={onPasswordHandler}
+            placeholder="비밀번호를 입력해주세요."
+          />
         </div>
-        <div style={{ marginBottom: "50px" }}>
-          <button type="submit">회원가입하기</button>
+        <div style={{ marginBottom: '50px' }}>
+          <label>비밀번호 확인</label>
+          <input
+            name="passwordConfirm"
+            value={passwordConfirm}
+            onChange={onConfirmPasswordHandler}
+            placeholder="비밀번호 한번더 입력해주세요."
+          />
+        </div>
+        <div>
+          <label>전화번호</label>
+          <input
+            name="phone"
+            value={phone}
+            onChange={onPhoneHandler}
+            placeholder="전화번호를 입력해주세요."
+          ></input>
+        </div>
+
+        <br />
+        <div style={{ marginBottom: '50px' }}>
+          <button type="submit">회원가입</button>
         </div>
       </form>
     </SLayout>
