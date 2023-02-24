b0d77eb605ee3b0353d72bb90569d12c57506e84
yeojin.kim <yeojin.kim@yanolja.com>
2023-02-24 21:24:40 +0900

    axiosInstance 예제 추가

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+const excludeTokenURL = ["/login", "/register"];
 
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("userToken");
 
-  if (token) {
+  if (!excludeTokenURL.includes(config.url) && token) {
     // eslint-disable-next-line no-param-reassign
     config.headers.token = token;
   }
